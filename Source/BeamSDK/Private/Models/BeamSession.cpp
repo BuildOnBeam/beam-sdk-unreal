06bde51f7e319f27b772e7f9d677b0a02beb92b7
sc-jstribling <jstribling@squareandcompassgames.com>
2024-08-21 14:47:57 -0500

    feat: update BeamSession with some additional JSON read/write support, use UTC for time, Iso8601 for serialization, and init from API response
    Also added FBeamSessionAndKeyPair struct as a replacement for the TTuple/kvp when getting the session and keys as a set

--- Source/BeamSDK/Private/Models/BeamSession.cpp
+++ Source/BeamSDK/Private/Models/BeamSession.cpp
@@ -2,10 +2,27 @@
 
 #include "BeamUtils.h"
 
+#include "BeamPlayerClientAPI.h"
+
 #include "PlayerClientHelpers.h"
 using OpenAPI::ParseDateTime;
 
 DEFINE_LOG_CATEGORY_STATIC(LogBeamSession, Log, All);
+
+FBeamSession::FBeamSession() {}
+
+FBeamSession::FBeamSession(FString& InJsonString)
+{
+	FromJson(InJsonString, *this);
+}
+
+FBeamSession::FBeamSession(PlayerClientSessionsApi::GetActiveSessionResponse& ApiResponse)
+{
+	Id = ApiResponse.Content.Id;
+	SessionAddress = ApiResponse.Content.SessionAddress;
+	StartTime = ApiResponse.Content.StartTime;
+	EndTime = ApiResponse.Content.EndTime;
+}
 
 bool FBeamSession::FromJson(const FString& InJsonString, FBeamSession& OutBeamSession)
 {
@@ -24,6 +41,33 @@ bool FBeamSession::FromJson(const FString& InJsonString, FBeamSession& OutBeamSe
 bool FBeamSession::FromJson(const FString& InJsonString)
 {
 	return FromJson(InJsonString, *this);
+}
+
+FString FBeamSession::WriteJson()
+{
+	FString jsonString = FString::Printf(TEXT(
+		"{"
+		"\"id\": \"%s\","
+		"\"sessionAddress\": \"%s\","
+		"\"startTime\": \"%s\","
+		"\"endTime\": \"%s\""
+		"}"),
+		*Id,
+		*SessionAddress,
+		*StartTime.ToIso8601(),
+		*EndTime.ToIso8601());
+	return jsonString;
+}
+
+bool FBeamSession::IsValidNow()
+{
+	auto now = FDateTime::UtcNow();
+	return StartTime <= now && EndTime > now;
+}
+
+bool FBeamSession::IsOwnedBy(KeyPair keyPair)
+{
+	return SessionAddress.Equals(keyPair.GetAddress().c_str(), ESearchCase::IgnoreCase);
 }
 
 
@@ -78,4 +122,31 @@ void FBeamSession::Test_Basic()
 	);
 	const FString testName = FString::Printf(TEXT("%hs"), __FUNCTION__);
 	Test(testName, testJson);
+}
+
+void FBeamSession::Test_ReadWrite()
+{
+	FDateTime StartTime = FDateTime::UtcNow();
+	FDateTime EndTime = FDateTime::UtcNow() + FTimespan(0, 5, 0);
+
+	FBeamSession TestSession;
+	TestSession.Id =TEXT("clTEST000000000000000TEST");
+	TestSession.SessionAddress = TEXT("0xTEST00000000000000000000000000000000TEST");
+	TestSession.StartTime = StartTime;
+	TestSession.EndTime = EndTime;
+	FString TestJson = TestSession.WriteJson();
+	UE_LOG(LogBeamSession, Log, TEXT("TestJson: %s"), *TestJson);
+
+	FBeamSession TestSession2;
+	TestSession2.FromJson(TestJson);
+	UE_LOG(LogBeamSession, Log, TEXT("EndTime: %s"), *TestSession2.EndTime.ToIso8601());
+
+	if (TestSession2.EndTime == TestSession.EndTime)
+	{
+		UE_LOG(LogBeamSession, Log, TEXT("----- ReadWrite - PASS -----."));
+	}
+	else
+	{
+		UE_LOG(LogBeamSession, Log, TEXT("----- ReadWrite - FAIL -----."));
+	}
 }
--- Source/BeamSDK/Public/Models/BeamSession.h
+++ Source/BeamSDK/Public/Models/BeamSession.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "Misc/DateTime.h"
+#include "Misc/Optional.h"
+#include "KeyPair.h"
 
 #include "BeamSession.generated.h"
 
@@ -23,11 +25,28 @@ public:
 	FDateTime EndTime;
 
 public:
+	FBeamSession();
+	FBeamSession(FString& InJsonString);
+	FBeamSession(class PlayerClientSessionsApi::GetActiveSessionResponse& ApiResponse);
+
+public:
 	static bool FromJson(const FString& InJsonString, FBeamSession& OutBeamSession);
 
 	bool FromJson(const FString& InJsonString);
+	FString WriteJson();
+
+	bool IsValidNow();
+	bool IsOwnedBy(KeyPair keyPair);
 
 public:
 	static void Test(const FString& TestName, const FString& TestJson);
 	static void Test_Basic();
+	static void Test_ReadWrite();
 };
+
+struct FBeamSessionAndKeyPair
+{
+	TOptional<FBeamSession> BeamSession;
+	KeyPair KeyPair;
+};
+
